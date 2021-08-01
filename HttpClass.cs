a135648817dccbbc682eb2d3efc09a716566e1d5
Chichimokers <87203631+Chichimokers@users.noreply.github.com>
2021-08-02 01:38:22 +0200

    Add files via upload

--- HttpClass.cs
+++ HttpClass.cs
@@ -9,9 +9,18 @@ namespace TodusApi
 {
     public static class HttpClass
     {
+        public static void DowlandFile(string URL,string token)
+        {
+            WebClient dowland = new WebClient();
+            dowland.Headers.Set("Method", "Get");
+            dowland.Headers.Set("authorization","Bearer "+token);
+            dowland.Headers.Set("Pragma", "no-cache");             
+            dowland.DownloadFile(URL, "main.rar");
+
+        }
         public static HttpWebResponse POST(string url,string data)
         {
-            var request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             byte[] bytess = Encoding.GetEncoding("ISO-8859-1").GetBytes(data);
             request.Method = "POST";
             request.Host = "auth.todus.cu";
